b9a9d418448b2e2f4e058dc6fc64bba82624fd24
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-06-18 20:31:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let hourEle=document.getElementById("hour")
+let minEle=document.getElementById("min")
+let secEle=document.getElementById("sec")
+let ampm=document.getElementById("ampm")
+
+setInterval(()=>{
+    let date=new Date
+    let hour=date.getHours()
+    let min=date.getMinutes()
+    let sec=date.getSeconds()
+    let ampm = hour >= 12 ? 'pm' : 'am';
+    hour = hour % 12;
+    hour = hour ? hour : 12;
+    hourEle.innerHTML=`${hour<10?'0'+hour:hour}`
+    minEle.innerHTML=`${min<10?'0'+min:min}`
+    secEle.innerHTML=`${sec<10?'0'+sec:sec}`
+    ampm.innerHTML=`${ampm}`
+    
+})
