825f512662283bdd37d91a9cf9d70b338dd38eea
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-06-18 20:37:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ setInterval(()=>{
     let hour=date.getHours()
     let min=date.getMinutes()
     let sec=date.getSeconds()
-    let ampm = hour >= 12 ? 'pm' : 'am';
+    let ampms = hour >= 12 ? 'pm' : 'am';
     hour = hour % 12;
     hour = hour ? hour : 12;
     hourEle.innerHTML=`${hour<10?'0'+hour:hour}`
     minEle.innerHTML=`${min<10?'0'+min:min}`
     secEle.innerHTML=`${sec<10?'0'+sec:sec}`
-    ampm.innerHTML=`${ampm}`
+    ampm.innerHTML=`${ampms}`
     
 })
